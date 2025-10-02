881ae924bf0ecb23b4403ba8180eeb58bae75327
Philipp Schuster <phip1611@gmail.com>
2025-10-02 15:54:49 +0200

    slides: make QrCode clickable

--- 2025-10-10-eurorust-minimal-rust-kernel/components/QrCode.vue
+++ 2025-10-10-eurorust-minimal-rust-kernel/components/QrCode.vue
@@ -1,5 +1,13 @@
 <template>
-  <qrcode-vue
+  <a v-if="isLink" :href="props.value" target="_blank"><qrcode-vue
+    :value="props.value"
+    :level="'L'"
+    :size="props.size ?? 100"
+    :render-as="'svg'"
+    :background="'#ffffff'"
+    :foreground="'#000000'"
+  /></a>
+  <qrcode-vue v-else
     :value="props.value"
     :level="'L'"
     :size="props.size ?? 100"
@@ -10,9 +18,14 @@
 </template>
 <script setup lang="ts">
 import QrcodeVue from 'qrcode.vue'
+import {computed} from "vue";
 
 const props = defineProps<{
   value: string
   size?: number
 }>()
+
+const isLink = computed<boolean>(() => {
+  return props.value.startsWith("http://") || props.value.startsWith("https://")
+})
 </script>
