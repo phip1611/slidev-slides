6056f98a78c1bf76d8b7b21bf5d47cfaa6fad3fe
Philipp Schuster <phip1611@gmail.com>
2026-05-19 08:01:08 +0200

    nix: update pnpm deps to fetcher version 3
    This fixes a warning with the upcoming nixos-26.05.

--- nix/build.nix
+++ nix/build.nix
@@ -29,7 +29,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   pnpmDeps = fetchPnpmDeps {
     inherit (finalAttrs) pname version src;
-    fetcherVersion = 2;
+    fetcherVersion = 3;
     hash = depHash;
   };
 
